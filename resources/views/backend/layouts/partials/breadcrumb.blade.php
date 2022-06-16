a2ff4b758e6fd13a24080b7569b44bcae75ecc88
dikshatechnofy <diksha.saini@hashstudioz.com>
2022-06-16 18:56:59 +0530

    final commit

--- resources/views/backend/layouts/partials/breadcrumb.blade.php
+++ resources/views/backend/layouts/partials/breadcrumb.blade.php
@@ -3,7 +3,7 @@
         <div class="row mb-2">
           <div class="col-sm-6">
         
-            <h1>{{ $title }}</h1>
+            <h1>{{ $title ?? '' }}</h1>
            
           </div>
           <div class="col-sm-6">
