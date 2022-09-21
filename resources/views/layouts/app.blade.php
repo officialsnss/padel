4cb5f7fd5c30d1df9369f16c41c2626ee843deda
NavjotS <navjot.singh@hashstudioz.com>
2022-09-21 12:32:16 +0530

    First commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,41 +1,187 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Sports Arena') }}</title>
-
-    <!-- Scripts -->
-
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-      <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{ asset('assets/plugins/fontawesome-free/css/all.min.css')}}">
-  <!-- icheck bootstrap -->
-  <link rel="stylesheet" href="{{ asset('assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="{{ asset('assets/backend/css/adminlte.min.css')}}">
-
-    <!-- Styles -->
-
+    <meta charset="UTF-8">
+    <title>TBAREE</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1" />
+    <link rel="apple-touch-icon" sizes="57x57" href="http://retalkapp.com/tbaree/01/favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="http://retalkapp.com/tbaree/01/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="http://retalkapp.com/tbaree/01/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="http://retalkapp.com/tbaree/01/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="http://retalkapp.com/tbaree/01/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="http://retalkapp.com/tbaree/01/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="http://retalkapp.com/tbaree/01/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="http://retalkapp.com/tbaree/01/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="http://retalkapp.com/tbaree/01/favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="http://retalkapp.com/tbaree/01/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="http://retalkapp.com/tbaree/01/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="http://retalkapp.com/tbaree/01/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="http://retalkapp.com/tbaree/01/favicon/favicon-16x16.png">
+    <link rel="manifest" href="http://retalkapp.com/tbaree/01/favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="favicon/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+    <link href="http://retalkapp.com/tbaree/01/css/jpreloader.css" rel="stylesheet" type="text/css" media="screen" />
+    <link href="http://retalkapp.com/tbaree/01/css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all" />
+    <link rel="stylesheet" type="text/css" href="http://retalkapp.com/tbaree/01/fancybox-master/jquery.fancybox.min.css" />
+    <link href="http://retalkapp.com/tbaree/01/swiper/swiper.min.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css" rel="stylesheet" />
+    <!--<link href="css/mouse.css" rel="stylesheet" type="text/css" media="all" />-->
+    <link href="http://retalkapp.com/tbaree/01/scrollbar/jquery.mCustomScrollbar.min.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="http://retalkapp.com/tbaree/01/css/scroll.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="http://retalkapp.com/tbaree/01/css/jquery-ui.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="http://retalkapp.com/tbaree/01/css/webslidemenu.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="http://retalkapp.com/tbaree/01/css/tbaree.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="http://retalkapp.com/tbaree/01/css/responsive.css" rel="stylesheet" type="text/css" media="all" />
 </head>
-<body class="hold-transition login-page">
-    <div id="app">
-        <main class="py-4">
+
+<body>
+    <header>
+        <div class="top-header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 topnnavi">
+                        <h5 class="top-follow-head">Follow With Us : </h5>
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-lg-4 col-md-6 col-sm-12 d-flex justify-content-between">
+                                    <ul class="list-inline mb-0 top-social">
+                                        <li class="list-inline-item"><a href="#"><i
+                                                    class="bi bi-facebook"></i></a></li>
+                                        <li class="list-inline-item"><a href="#"><i
+                                                    class="bi bi-twitter"></i></a></li>
+                                        <li class="list-inline-item"><a href="#"><i
+                                                    class="bi bi-instagram"></i></a></li>
+                                        <li class="list-inline-item"><a href="#"><i
+                                                    class="bi bi-youtube"></i></a></li>
+                                        <li class="list-inline-item"><a href="#"><i
+                                                    class="bi bi-linkedin"></i></a></li>
+                                    </ul>
+                                </div>
+                                <div class="col-lg-4 col-md-4 tbaree-col-mobile">
+                                    <div class="tbaree-logo text-center">
+                                        <img src="http://retalkapp.com/tbaree/01/images/tbaree-logo.png" class="img-fluid" alt="Tbaree">
+                                    </div>
+                                </div>
+
+
+                                <div class="col-lg-4 col-md-6 col-sm-12">
+                                    <div class="login-wish-account">
+                                        <ul>
+                                            <li class="account-main">
+                                                <a href="javascript:void(0);" class="account-link"><img
+                                                        src="http://retalkapp.com/tbaree/01/images/my-account.svg" alt="My Account"><span>My
+                                                        Account</span></a>
+                                                <div class="account-div">
+                                                    <ul class="account-ul">
+                                                        <li><a href="javscript:void(0)">Login</a></li>
+                                                        <li><a href="javscript:void(0)">Register</a></li>
+                                                    </ul>
+                                                </div>
+                                            </li>
+                                            <li class="list-inline-item">
+                                                <div class="search-main">
+                                                    <a href="javascript:void(0);" class="search-link"><img
+                                                            class="icon-close" src="http://retalkapp.com/tbaree/01/images/icon-close.png"
+                                                            alt="close"><img class="icon-search"
+                                                            src="http://retalkapp.com/tbaree/01/images/icon-search.svg" alt="search"> Search</a>
+                                                    <form name="Searchform" action="searchresults.php"
+                                                        method="post">
+                                                        <div class="search-sub" style="display: none;">
+                                                            <div class="search-div">
+                                                                <div class="search-input">
+                                                                    <input required="" name="searchword"
+                                                                        type="search" class="form-control"
+                                                                        placeholder="Search here..." value=""
+                                                                        minlength="3">
+                                                                    <button type="submit"
+                                                                        name="submitsearch">GO</button>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </form>
+                                                </div>
+                                            </li>
+                                            <li class="list-inline-item">
+                                                <a href="ar/index.php" class="arabic-lang">العربية </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 navibar">
+            <div class="navigation container-fluid">
+                <div class="wsmenucontainer clearfix">
+                    <div class="overlapblackbg"></div>
+                    <div class="wsmobileheader clearfix">
+                        <a id="wsnavtoggle" class="animated-arrow"><span></span></a>
+                    </div>
+                    <div class="wsmain">
+                        <nav class="wsmenu clearfix">
+                            <ul class="mobile-sub wsmenu-list">
+                                <li><a href="javascript:void(0);" class="active">Home</a></li>
+                                <li><a href="javascript:void(0);">Players</a></li>
+                                <li><a href="javascript:void(0);">Booking</a></li>
+                                <li><a href="javascript:void(0);">Games</a></li>
+                                <li><a href="javascript:void(0);">Coaches</a></li>
+                                <li><a href="javascript:void(0);">About Us</a></li>
+                                <li><a href="javascript:void(0);">Contact Us</a></li>
+
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <div id="viewport">
+        <div id="scroll-container">
+            <div class="tbaree-logo tbaree-logo-mobile text-center">
+                <img src="http://retalkapp.com/tbaree/01/images/tbaree-logo.png" class="img-fluid" alt="Tbaree">
+            </div>
+
             @yield('content')
-        </main>
+
+            <footer>
+                <div class="padding-left-right">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-12">
+                                <p class="copyright">Copyright 2022-2023 - Tbaree. All Rights Reserved</p>
+                                <p class="designed">Managed by: <a href="https://designmasterevents.com"
+                                        target="_blank">Design Master Events</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+
+        </div>
     </div>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="{{asset('assets/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-<!-- AdminLTE App -->
-<script src="{{asset('assets/backend/js/adminlte.min.js')}}"></script>
+    <div class="shape-quick-wrapper"></div>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-migrate-1.4.1.min.js"></script>
+    <script src="http://retalkapp.com/tbaree/01/js/bootstrap.bundle.min.js"></script>
+    <script src="http://retalkapp.com/tbaree/01/js/jpreloader.js"></script>
+    <script src="http://retalkapp.com/tbaree/01/js/loader.js"></script>
+    <script src="http://retalkapp.com/tbaree/01/js/webslidemenu.js"></script>
+    <script src="http://retalkapp.com/tbaree/01/js/jquery-ui.min.js"></script>
+    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+    <script src="http://retalkapp.com/tbaree/01/swiper/swiper.js"></script>
+    <script type="text/javascript" src="http://retalkapp.com/tbaree/01/scrollbar/jquery.mCustomScrollbar.concat.min.js"></script>
+    <script src="http://retalkapp.com/tbaree/01/fancybox-master/jquery.fancybox.min.js"></script>
+    <script src="http://retalkapp.com/tbaree/01/fancybox-master/fancybox.js"></script>
+    <script src="http://retalkapp.com/tbaree/01/js/jquery.lavalamp.min.js"></script>
+    <script src="http://retalkapp.com/tbaree/01/js/custom.js"></script>
 </body>
+
 </html>
