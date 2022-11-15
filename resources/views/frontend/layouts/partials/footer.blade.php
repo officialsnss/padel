60219d6c27a52b6822a134db490dc40350ccdee3
Anmol Chugh <anmol.chugh@hashstudioz.com>
2022-11-15 10:31:20 +0530

    Footer code modified and added arabic to it

--- resources/views/frontend/layouts/partials/footer.blade.php
+++ resources/views/frontend/layouts/partials/footer.blade.php
@@ -1,24 +1,9 @@
 <div class="footer-wrap">
     <div class="container">
         <div class="row justify-content-between">
-            @php
-                $copyrights = \App\Models\Setting::where('type', '2')->get();
-            @endphp
-            @foreach ($copyrights as $copyright)
-                @if ($copyright->value != '')
-                    <div class="col-auto">{{ $copyright->value }}</div>
-                @endif
-            @endforeach
-
-            <div class="col-auto term-policy"><a href="/pages/terms-and-condition">Terms & Condition</a> <a href="/pages/refund-policy">Refund Policy</a> <a href="/pages/privacy-policy">Privacy Policy</a></div>
-            @php
-                $extra_texts = \App\Models\Setting::where('type', '3')->get();
-            @endphp
-            @foreach ($extra_texts as $extra_text)
-                @if ($extra_text->value != '')
-                <div class="col-auto"><a href="https://www.design-master.com/" target="_blank">{{ $extra_text->value }}</a></div>
-                @endif
-            @endforeach
+            <div class="col-auto">{{ (__('home.footer.copyright_text')) }}</div>
+            <div class="col-auto term-policy"><a href="/pages/terms-and-condition">{{ (__('home.footer.terms_and_condition')) }}</a> <a href="/pages/refund-policy">{{ (__('home.footer.refund_policy')) }}</a> <a href="/pages/privacy-policy">  {{ (__('home.footer.privacy_policy')) }}</a></div>
+            <div class="col-auto"><a href="https://www.design-master.com/" target="_blank">{{ (__('home.footer.footer_extra_text')) }}</a></div>
         </div>
     </div>
 </div>
