c6716affd9b32d109aa2bcc277f21dc7dfcafc5e
Anmol Chugh <anmol.chugh@hashstudioz.com>
2022-11-17 13:58:15 +0530

    Issues in add player api is fixed

--- resources/views/frontend/pages/courts-book.blade.php
+++ resources/views/frontend/pages/courts-book.blade.php
@@ -167,13 +167,14 @@
     </div>
 </div>
 
-    <!--Add Player-->
-    <div class="modal fade" id="add-player" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
 
-        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <div class="w-100">
+<!--Add Player-->
+<div class="modal fade" id="add-player" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+
+<div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+    <div class="modal-content">
+        <div class="modal-header">
+            <div class="w-100">
                         <h1 class="modal-title w-100 fs-5 mb-3 position-relative" id="exampleModalLabel">
                         @if (App::getLocale() == 'en')
                             Add Player
@@ -193,24 +194,29 @@
                             </button></div>
                     </div>
 
-    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-        <div class="modal-content">
-            <div class="modal-header">
-                <div class="w-100">
-                    <h1 class="modal-title w-100 fs-5 mb-3 position-relative" id="exampleModalLabel">Add Player
-                        <button type="button" class="btn-close top-0 end-0 position-absolute" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </h1>
-                    <div class="form-group w-100 search-input mb-1 position-relative"><input type="text" class="form-control" placeholder="Search Players" /><button class="btn button position-absolute">Search</button></div>
+        </div>
+        <div class="modal-body">
+            @foreach ($getPlayers as $getPlayer)
+                <div class="add-player-row">
+                    <div class="row g-4 align-items-center">
+                        <div class="col-auto">
+                            <div class="add-player-img"><img src="{{ asset('/images/player_images') }}/{{$getPlayer->profile_pic}}" alt=""
+                                    class="img-fluid"></div>
+                        </div>
+                        <div class="col">
+                            @if (App::getLocale() == 'en')
+                                <h6>{{$getPlayer->name}}</h6>
+                            @else
+                                <h6>{{$getPlayer->name_arabic}}</h6>
+                            @endif
+                        </div>
+                        <div class="col-auto"><a class="btn btn-dark" href="#" role="button">Add</a></div>
+                    </div>
                 </div>
-                <div class="modal-body">
-                <div class="row g-4 list-players">
-
-                </div>
-                </div>
-                @endforeach
-            </div>
+            @endforeach
         </div>
     </div>
+</div>
 </div>
 <!--Add Player-->
 
